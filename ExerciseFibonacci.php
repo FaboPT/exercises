b5cdef2719b6d8f71e68884240fdd85255772b32
Fábio Galvão <fgalvao90@gmail.com>
2021-09-04 14:38:44 +0100

    Add iterable function fibonacci

--- ExerciseFibonacci.php
+++ ExerciseFibonacci.php
@@ -2,11 +2,25 @@
 
 function fibonacci(int $n)
 {
-    if($n === 0)
-        return 0;
-    return $n<3 ? 1 : fibonacci($n - 1) + fibonacci($n - 2);
+    return $n<2 ? $n : fibonacci($n - 1) + fibonacci($n - 2);
 }
+
+function fib(int $n){
+    $j = 1;
+    $i = 0;
+    
+    for($k=0;$k<$n; $k++){
+        $t = $i+$j;
+        $i = $j;
+        $j = $t;
+    }
+    return $i;
+}
+
 
 echo fibonacci(10) ."\n";
 echo fibonacci(5) ."\n";
 echo fibonacci(1) ."\n";
+echo fib(10) ."\n";
+echo fib(5) ."\n";
+echo fib(1) ."\n";
